42db2ceae19d63b508fadd8c36b7b78da7c59063
Jemin Lad <jeminlad2003@gmail.com>
2025-03-20 09:45:24 +0530

    changes

--- MVC/obj/Debug/net8.0/MVC.AssemblyInfo.cs
+++ MVC/obj/Debug/net8.0/MVC.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9b71bef313991f4debdce77ee551bacbafaa603e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b5e86e4aff1496a6000505dfdb5548c39919ad28")]
 [assembly: System.Reflection.AssemblyProductAttribute("MVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
